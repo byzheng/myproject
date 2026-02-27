7f9c17b0dd15f9ccd9caf8d8296cabe0ac1b6c3b
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-02-27 14:05:16 +1000

    Add new path function

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,36 @@
 
 # Helper function to convert absolute path to relative
-make_relative <- function(abs_path, root_dir) {
-    stopifnot(is.character(abs_path), length(abs_path) > 0)
-    stopifnot(is.character(root_dir), length(root_dir) == 1)
-    root_dir <- suppressWarnings(normalizePath(root_dir, winslash = "/"))
-    
-    rel_path <- c()
-    for (i in seq_along(abs_path)) {
-        abs_path_i <- suppressWarnings(normalizePath(abs_path[i], winslash = "/"))
-    
-        if (!grepl(paste0("^", gsub("\\\\", "\\\\\\\\", root_dir)), abs_path_i)) {
-            return(abs_path_i) # Not under root, return as-is
+make_relative <- function(target, start) {
+    stopifnot(is.character(target), length(target) > 0)
+    stopifnot(is.character(start), length(start) == 1)
+    start <- suppressWarnings(normalizePath(start, mustWork = FALSE, winslash = "/"))
+    start_parts <- strsplit(start, "/", fixed = TRUE)[[1]]
+
+    rel_paths <- c()
+    for (i in seq_along(target)) {
+        target_norm <- normalizePath(target[i], mustWork = FALSE, winslash = "/")    
+        target_parts <- strsplit(target_norm, "/", fixed = TRUE)[[1]]
+        
+        if (length(target_parts) > 0 && length(start_parts) > 0 &&
+            !identical(tolower(target_parts[[1]]), tolower(start_parts[[1]]))) {
+            return(target_norm)
         }
-        
-        # Remove root prefix and leading slash
-        rel_path_i <- sub(paste0("^", gsub("\\\\", "\\\\\\\\", root_dir), "/?"), "", abs_path_i)
-        
-        rel_path[i] <- rel_path_i
-        
+
+        common <- 0L
+        max_common <- min(length(target_parts), length(start_parts))
+        while (common < max_common &&
+            identical(tolower(target_parts[[common + 1L]]), tolower(start_parts[[common + 1L]]))) {
+            common <- common + 1L
+        }
+
+        up <- if (common < length(start_parts)) rep("..", length(start_parts) - common) else character(0)
+        down <- if (common < length(target_parts)) target_parts[(common + 1L):length(target_parts)] else character(0)
+        rel <- c(up, down)
+
+        if (length(rel) == 0) {
+            rel <- "."
+        }
+        rel_paths <- c(rel_paths, do.call(file.path, as.list(rel)))
     }
-    return(rel_path)
+    return(rel_paths)
 }
